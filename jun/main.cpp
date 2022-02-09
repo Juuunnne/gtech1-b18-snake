872825218a3ecff01e71b779ee6699c8139800fa
Juuunnne <jzhi@gaming.tech>
2022-02-09 19:58:52 +0100

    merge fix

---
+++ jun/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "snake.hpp"
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    Game game = Game();
+    Game().Run();
+    cout << "Game has terminated successfully" << endl;
+    return 0;
+}
